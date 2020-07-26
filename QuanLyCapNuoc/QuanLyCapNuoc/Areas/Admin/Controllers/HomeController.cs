c090784643ad36594c3547286754492a1a14b7e7
Nghia <minhnghia2311@gmail.com>
2020-07-26 20:44:20 +0700

    create layout Admin

---
+++ QuanLyCapNuoc/QuanLyCapNuoc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace QuanLyCapNuoc.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult error404()
+        {
+            return View();
+        }
+        public ActionResult error500()
+        {
+            return View();
+        }
+    }
+}
